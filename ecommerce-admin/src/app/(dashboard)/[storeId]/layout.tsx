4088784a81b089090d145240ef3fb336a6fd753f
João Manoel <joaomanoeldev@gmail.com>
2023-12-17 17:17:10 -0300

    feat: create dashboard layout

---
+++ ecommerce-admin/src/app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,32 @@
+import { redirect } from "next/navigation"
+import { auth } from "@clerk/nextjs"
+
+import { prismadb } from "@/lib/prismadb"
+
+export default async function DashboardLayout ({
+  children,
+  params
+}: {
+  children: React.ReactNode,
+  params: { storeId: string }
+}) {
+  const { userId } = auth()
+
+  if (!userId) redirect("/sign-in")
+  
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      user_id: userId
+    }
+  })
+
+  if (!store) redirect("/")
+
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
