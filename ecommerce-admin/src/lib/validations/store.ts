c6e39fee7a5e955622f8499b7579fe372b7a9222
João Manoel <joaomanoeldev@gmail.com>
2023-12-22 13:54:55 -0300

    feat: create store editing validation

--- ecommerce-admin/src/lib/validations/store.ts
+++ ecommerce-admin/src/lib/validations/store.ts
@@ -5,3 +5,9 @@ export const newStoreSchema = z.object({
 })
 
 export type NewStoreInput = z.infer<typeof newStoreSchema>
+
+export const updateStoreSettingsSchema = z.object({
+  name: z.string().min(1, "O nome não pode estar vazio")
+})
+
+export type UpdateStoreSettingsInput = z.infer<typeof updateStoreSettingsSchema>
