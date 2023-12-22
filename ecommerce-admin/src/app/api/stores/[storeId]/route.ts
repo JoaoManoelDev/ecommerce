4f35a14c5ef8d4892b193cbea6764e5bf15b3eda
João Manoel <joaomanoeldev@gmail.com>
2023-12-22 13:53:16 -0300

    feat: create route for editing and deleting stores by id

---
+++ ecommerce-admin/src/app/api/stores/[storeId]/route.ts
@@ -0,0 +1,70 @@
+import { NextResponse } from "next/server"
+import { auth } from "@clerk/nextjs"
+
+import { prismadb } from "@/lib/prismadb"
+
+interface RouteParams {
+  params: {
+    storeId: string
+  }
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: RouteParams
+) {
+  try {
+    const { userId } = auth()
+    
+    if (!userId) return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+
+    if (!params.storeId) NextResponse.json({ message: "Store id is required" }, { status: 400 })
+
+    const body = await request.json()
+    const { name } = body
+
+    if (!name) NextResponse.json({ message: "Name is required" }, { status: 400 })
+
+    const store = await prismadb.store.updateMany({
+      where: {
+        id: params.storeId,
+        user_id: userId
+      },
+      data: {
+        name
+      }
+    })
+
+    return NextResponse.json(store, { status: 201 })
+
+  } catch (error) {
+    console.log("[STORE_PATCH]", error)
+    return NextResponse.json({ message: "Internal error." }, { status: 500 })
+  }
+}
+
+export async function DELETE(
+  _request: Request,
+  { params }: RouteParams
+) {
+  try {
+    const { userId } = auth()
+    
+    if (!userId) return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+
+    if (!params.storeId) NextResponse.json({ message: "Store id is required" }, { status: 400 })
+
+    await prismadb.store.deleteMany({
+      where: {
+        id: params.storeId,
+        user_id: userId
+      }
+    })
+
+    return NextResponse.json({ message: "Store deleted successfully" })
+
+  } catch (error) {
+    console.log("[STORE_DELETE]", error)
+    return NextResponse.json({ message: "Internal error." }, { status: 500 })
+  }
+}
