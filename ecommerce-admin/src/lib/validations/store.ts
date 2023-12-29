2d212490b26191519d2467d4bcf285791c99fb64
João Manoel <joaomanoeldev@gmail.com>
2023-12-28 22:42:15 -0300

    chore: rename schema for storeSchema and remove updateStoreSettingsSchema

--- ecommerce-admin/src/lib/validations/store.ts
+++ ecommerce-admin/src/lib/validations/store.ts
@@ -1,13 +1,7 @@
 import * as z from "zod"
 
-export const newStoreSchema = z.object({
-  name: z.string().min(1, "O nome da loja não pode estar vazio.")
+export const storeSchema = z.object({
+  name: z.string()
 })
 
-export type NewStoreInput = z.infer<typeof newStoreSchema>
-
-export const updateStoreSettingsSchema = z.object({
-  name: z.string().min(1, "O nome não pode estar vazio")
-})
-
-export type UpdateStoreSettingsInput = z.infer<typeof updateStoreSettingsSchema>
+export type StoreInput = z.infer<typeof storeSchema>
