4f4f16f59234dd16d1c976bd2a3cfc5f97a6180d
João Manoel <joaomanoeldev@gmail.com>
2024-01-09 18:38:26 -0300

    feat: fetch all billboards in the active store

--- ecommerce-admin/src/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ ecommerce-admin/src/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,10 +1,37 @@
 import { BillboardClient } from "./components/client"
+import { BillboardColumn } from "./components/columns"
 
-export default function BillboardsPage() {
+import { prismadb } from "@/lib/prismadb"
+import { dateFormatter } from "@/lib/formatter"
+
+interface BillboardsPageProps {
+  params: {
+    storeId: string
+  }
+}
+
+export default async function BillboardsPage({
+  params
+}: BillboardsPageProps) {
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      store_id: params.storeId
+    },
+    orderBy: {
+      created_at: "desc"
+    }
+  })
+
+  const formattedBillboards: BillboardColumn[] = billboards.map((billboard) => ({
+    id: billboard.id,
+    label: billboard.label,
+    createdAt: dateFormatter(billboard.created_at)
+  }))
+
   return (
     <div className="flex flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient />
+        <BillboardClient data={formattedBillboards} />
       </div>
     </div>
   )
