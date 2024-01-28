2b2fbe26a4cf03ffd851fe590ecdbf41359af820
João Manoel <joaomanoeldev@gmail.com>
2024-01-28 17:41:58 -0300

    feat: add categories route

--- ecommerce-admin/src/components/navbar/content.tsx
+++ ecommerce-admin/src/components/navbar/content.tsx
@@ -18,6 +18,11 @@ export const NavbarContent = ({
       active: pathname === `/${params.storeId}`,
     },
     {
+      href: `/${params.storeId}/categories`,
+      label: "Categorias",
+      active: pathname === `/${params.storeId}/categories`,
+    },
+    {
       href: `/${params.storeId}/billboards`,
       label: "Outdoors",
       active: pathname === `/${params.storeId}/billboards`,
