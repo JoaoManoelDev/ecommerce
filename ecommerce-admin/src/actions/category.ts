585c4d09292cd0f12609c66916b5f8c1bf6cac1c
João Manoel <joaomanoeldev@gmail.com>
2024-01-28 17:40:24 -0300

    feat: create getCategoriesByStoreId and getCategoryById action

---
+++ ecommerce-admin/src/actions/category.ts
@@ -0,0 +1,27 @@
+import { prismadb } from "@/lib/prismadb"
+
+export const getCategoriesByStoreId = async (storeId: string) => {
+  const categories = prismadb.category.findMany({
+    where: {
+      store_id: storeId
+    },
+    include: {
+      billboard: true
+    },
+    orderBy: {
+      created_at: "desc"
+    }
+  })
+
+  return categories
+}
+
+export const getCategoryById = async (categoryId: string) => {
+  const category = await prismadb.category.findUnique({
+    where: {
+      id: categoryId
+    }
+  })
+
+  return category
+}
