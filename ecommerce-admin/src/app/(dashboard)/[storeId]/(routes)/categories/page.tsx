86bf9219304b50bba7487a74b4bb37293e506cc5
João Manoel <joaomanoeldev@gmail.com>
2024-01-30 16:41:08 -0300

    feat: create category page

---
+++ ecommerce-admin/src/app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -0,0 +1,32 @@
+import { CategoryClient } from "./components/client"
+import { CategoryColumn } from "./components/columns"
+
+import { dateFormatter } from "@/lib/formatter"
+import { getCategoriesByStoreId } from "@/actions/category"
+
+interface CategoryPageProps {
+  params: {
+    storeId: string
+  }
+}
+
+export default async function CategoryPage({
+  params
+}: CategoryPageProps) {
+  const categories = await getCategoriesByStoreId(params.storeId)
+
+  const clientFormattedCategories = categories.map<CategoryColumn>((category) => ({
+    id: category.id,
+    name: category.name,
+    billboardLabel: category.billboard.label,
+    createdAt: dateFormatter(category.created_at),
+  }))
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CategoryClient data={clientFormattedCategories} />
+      </div>
+    </div>
+  )
+}
