5ddbc697ab646a64f3b14dfb6e2d3cddef3c2e1e
João Manoel <joaomanoeldev@gmail.com>
2024-02-12 22:25:20 -0300

    feat: create sizes page

---
+++ ecommerce-admin/src/app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -0,0 +1,32 @@
+import { SizeClient } from "./components/client"
+import { SizeColumn } from "./components/columns"
+
+import { dateFormatter } from "@/lib/formatter"
+import { getSizeByStoreId } from "@/actions/sizes"
+
+interface SizesPageProps {
+  params: {
+    storeId: string
+  }
+}
+
+export default async function SizesPage({
+  params
+}: SizesPageProps) {
+  const sizes = await getSizeByStoreId(params.storeId)
+
+  const formattedSizes: SizeColumn[] = sizes.map((size) => ({
+    id: size.id,
+    name: size.name,
+    value: size.value,
+    createdAt: dateFormatter(size.created_at)
+  }))
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SizeClient data={formattedSizes} />
+      </div>
+    </div>
+  )
+}
