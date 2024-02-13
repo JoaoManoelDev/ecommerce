878db0873d5a355ce7f4746f5982b1c1851fec6b
João Manoel <joaomanoeldev@gmail.com>
2024-02-13 18:07:34 -0300

    feat: create route to create and search all colors

---
+++ ecommerce-admin/src/app/api/[storeId]/colors/route.ts
@@ -0,0 +1,78 @@
+import { NextResponse } from "next/server"
+import { auth } from "@clerk/nextjs"
+
+import { prismadb } from "@/lib/prismadb"
+import { colorBodyValidation } from "@/app/api/validations/color"
+
+interface RouteParams {
+  params: {
+    storeId: string
+  }
+}
+
+export async function POST(
+  request: Request,
+  { params }: RouteParams
+) {
+  try {
+    const { userId } = auth()
+
+    if (!userId) return NextResponse.json({ message: "Unauthenticated." }, { status: 401 })
+
+    if (!params.storeId) return NextResponse.json({ message: "Store id is required." }, { status: 400 })
+
+    const storeExists = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        user_id: userId
+      }
+    })
+
+    if (!storeExists) return NextResponse.json({ message: "Store not found." }, { status: 404 })
+    
+    const body = await request.json()
+
+    const validation = colorBodyValidation.safeParse(body)
+
+    if (!validation.success) {
+      return NextResponse.json(validation.error.formErrors.fieldErrors, { status: 400 })
+    }
+
+    const { name, value } = validation.data
+
+    const newColor = await prismadb.color.create({
+      data: {
+        name,
+        value,
+        store_id: params.storeId,
+      }
+    })
+
+    return NextResponse.json(newColor, { status: 201 })
+
+  } catch (error) {
+    console.log("[CREATE_COLOR_ERROR]", error)
+    return NextResponse.json({ message: "Internal error." }, { status: 500 })
+  }
+}
+
+export async function GET(
+  _request: Request,
+  { params }: RouteParams
+) {
+  try {
+    if (!params.storeId) return NextResponse.json({ message: "Store id is required." }, { status: 400 })
+
+    const colors = await prismadb.color.findMany({
+      where: {
+        store_id: params.storeId
+      }
+    })
+
+    return NextResponse.json(colors, { status: 200 })
+
+  } catch (error) {
+    console.log("[GET_COLOR_ERROR]", error)
+    return NextResponse.json({ message: "Internal error." }, { status: 500 })
+  }
+}
