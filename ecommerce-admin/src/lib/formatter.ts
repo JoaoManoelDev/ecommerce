b4568d35ee2542b36bb242946cc7eb039e4582de
João Manoel <joaomanoeldev@gmail.com>
2024-02-15 19:11:56 -0300

    feat: create priceFormatter

--- ecommerce-admin/src/lib/formatter.ts
+++ ecommerce-admin/src/lib/formatter.ts
@@ -2,3 +2,10 @@ import { format } from "date-fns"
 import { ptBR } from "date-fns/locale"
 
 export const dateFormatter = (date: Date) => format(date, "dd 'de' MMMM", { locale: ptBR })
+
+export const priceFormatter = (price: number) => {
+  return Intl.NumberFormat("ptBR", {
+    style: "currency",
+    currency: "BRL"
+  }).format(price)
+}
