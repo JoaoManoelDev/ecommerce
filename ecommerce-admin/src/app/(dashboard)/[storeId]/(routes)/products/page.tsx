aff33883649cfb34197c23b390609daca3206e5b
João Manoel <joaomanoeldev@gmail.com>
2024-02-15 19:15:48 -0300

    feat: create products page

---
+++ ecommerce-admin/src/app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -0,0 +1,44 @@
+import { ProductClient } from "./components/client"
+import { ProductColumn } from "./components/columns"
+
+import { dateFormatter, priceFormatter } from "@/lib/formatter"
+import { getProductByStoreId } from "@/actions/products"
+
+interface ProductsPageProps {
+  params: {
+    storeId: string
+  }
+}
+
+export default async function ProductsPage({
+  params
+}: ProductsPageProps) {
+  const products = await getProductByStoreId({
+    storeId: params.storeId,
+    includes: {
+      category: true,
+      color: true,
+      size: true
+    }
+  })
+
+  const formattedProducts: ProductColumn[] = products.map((product) => ({
+    id: product.id,
+    name: product.name,
+    isFeature: product.is_feature,
+    isArchived: product.is_archived,
+    price: priceFormatter(product.price),
+    category: product.category.name,
+    size: product.size.name,
+    color: product.color.name,
+    createdAt: dateFormatter(product.created_at)
+  }))
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ProductClient data={formattedProducts} />
+      </div>
+    </div>
+  )
+}
