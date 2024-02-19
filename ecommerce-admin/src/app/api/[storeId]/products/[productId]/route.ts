6d0c8aae2f8f1d299479acc104cafa04cd135f1a
João Manoel <joaomanoeldev@gmail.com>
2024-02-19 15:54:53 -0300

    feat: create route to search, edit and delete a specific product

---
+++ ecommerce-admin/src/app/api/[storeId]/products/[productId]/route.ts
@@ -0,0 +1,156 @@
+import { NextResponse } from "next/server"
+import { auth } from "@clerk/nextjs"
+
+import { prismadb } from "@/lib/prismadb"
+import { productBodyValidation } from "@/app/api/validations/product"
+
+interface RouteParams {
+  params: {
+    storeId: string
+    productId: string
+  }
+}
+
+export async function GET(
+  _request: Request,
+  { params }: RouteParams
+) {
+  try {
+    if (!params.productId) NextResponse.json({ message: "Product ID is required" }, { status: 400 })
+
+    const product = await prismadb.product.findUnique({
+      where: {
+        id: params.productId,
+      },
+      include: {
+        image: true,
+        category: true,
+        size: true,
+        color: true
+      }
+    })
+
+    return NextResponse.json(product, { status: 200 })
+
+  } catch (error) {
+    console.log("[PRODUCT_GET]", error)
+    return NextResponse.json({ message: "Internal error." }, { status: 500 })
+  }
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: RouteParams
+) {
+  try {
+    const { userId } = auth()
+    
+    if (!userId) return NextResponse.json({ message: "Unauthenticated." }, { status: 401 })
+
+    if (!params.productId) NextResponse.json({ message: "Product ID is required." }, { status: 400 })
+
+    const body = await request.json()
+
+    const validation = productBodyValidation.safeParse(body)
+
+    if (!validation.success) {
+      return NextResponse.json(validation.error.formErrors.fieldErrors, { status: 400 })
+    }
+
+    const {
+      categoryId,
+      colorId,
+      images,
+      name,
+      price,
+      sizeId,
+      isArchived,
+      isFeature,
+    } = validation.data
+
+    const storeExists = prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        user_id: userId
+      }
+    })
+
+    if (!storeExists) return NextResponse.json({ message: "Store not found." }, { status: 404 })
+
+    const priceInCents = price * 100
+
+    await prismadb.product.update({
+      where: {
+        id: params.productId,
+      },
+      data: {
+        name,
+        price: priceInCents,
+        category_id: categoryId,
+        color_id: colorId,
+        size_id: sizeId,
+        is_archived: isArchived,
+        is_feature: isFeature,
+        store_id: params.storeId,
+        image: {
+          deleteMany: {}
+        }
+      }
+    })
+
+    const product = await prismadb.product.update({
+      where: {
+        id: params.productId
+      },
+      data: {
+        image: {
+          createMany: {
+            data: [
+              ...images.map(image => image)
+            ]
+          }
+        }
+      }
+    })
+
+    return NextResponse.json(product, { status: 200 })
+
+  } catch (error) {
+    console.log("[PRODUCT_PATCH]", error)
+    return NextResponse.json({ message: "Internal error." }, { status: 500 })
+  }
+}
+
+export async function DELETE(
+  _request: Request,
+  { params }: RouteParams
+) {
+  try {
+    const { userId } = auth()
+    
+    if (!userId) return NextResponse.json({ message: "Unauthorized." }, { status: 401 })
+
+    if (!params.productId) NextResponse.json({ message: "Product ID is required." }, { status: 400 })
+
+    const storeExists = prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        user_id: userId
+      }
+    })
+
+    if (!storeExists) return NextResponse.json({ message: "Store not found." }, { status: 404 })
+
+    await prismadb.product.deleteMany({
+      where: {
+        id: params.productId,
+      }
+    })
+
+    return NextResponse.json({ message: "Product deleted successfully." }, { status: 200 })
+
+  } catch (error) {
+    console.log("[PRODUCT_DELETE]", error)
+    return NextResponse.json({ message: "Internal error." }, { status: 500 })
+  }
+}
