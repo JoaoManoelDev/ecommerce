6575a8934555a85694695592683533ba41f745c9
João Manoel <joaomanoeldev@gmail.com>
2024-02-23 19:33:56 -0300

    feat: create getProducts action

---
+++ ecommerce-store/src/actions/products.ts
@@ -0,0 +1,35 @@
+import qs from "query-string"
+
+import { Product } from "@/entities/product"
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`
+
+interface Query {
+  categoryId?: string
+  colorId?: string
+  sizeId?: string
+  isFeatured?: boolean
+}
+
+export const getProducts = async (query?: Query):Promise<Product[]> => {
+  const url = qs.stringifyUrl({
+    url: URL,
+    query: {
+      categoryId: query?.categoryId,
+      colorId: query?.colorId,
+      sizeId: query?.sizeId,
+      isFeatured: query?.isFeatured,
+    }
+  })
+
+  const response = await fetch(url)
+
+  const products: Product[] | [] = await response.json()
+
+  const formattedProducts = products.map(product => ({
+    ...product,
+    price: product.price / 100
+  }))
+
+  return formattedProducts
+}
