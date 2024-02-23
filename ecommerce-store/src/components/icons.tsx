f43eaa1d12e59f89d737fa6cb7832d9d24c9a215
João Manoel <joaomanoeldev@gmail.com>
2024-02-23 19:43:38 -0300

    feat: create Icons and Carousel component

---
+++ ecommerce-store/src/components/icons.tsx
@@ -0,0 +1,15 @@
+import {
+  ShoppingBag,
+  Expand,
+  ShoppingBasket,
+  ArrowLeft,
+  ArrowRight
+} from "lucide-react"
+
+export const Icons = {
+  shoppingBag: ShoppingBag,
+  expand: Expand,
+  shoppingCart: ShoppingBasket,
+  arrowLeft: ArrowLeft,
+  arrowRight: ArrowRight
+}
