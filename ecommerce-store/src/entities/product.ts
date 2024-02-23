9e1b306460ac5113b5a3fa26a6f0c5dee45fb765
João Manoel <joaomanoeldev@gmail.com>
2024-02-23 19:21:42 -0300

    feat: create product entity

---
+++ ecommerce-store/src/entities/product.ts
@@ -0,0 +1,15 @@
+import { Category } from "@/entities/category"
+import { Color } from "@/entities/color"
+import { Size } from "@/entities/size"
+import { Image } from "@/entities/image"
+ 
+export interface Product {
+  id: string
+  category: Category
+  name: string
+  price: number
+  isFeatured: boolean
+  size: Size
+  color: Color
+  image: Image[]
+}
