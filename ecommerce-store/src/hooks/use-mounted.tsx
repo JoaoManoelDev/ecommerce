c29ad2b4243b6d3d288fd150466ddb0ca267e57c
João Manoel <joaomanoeldev@gmail.com>
2024-02-23 19:24:04 -0300

    feat: create useMounted hook

---
+++ ecommerce-store/src/hooks/use-mounted.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export function useMounted() {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+
+    return () => setMounted(false)
+  }, [])
+
+  return { mounted }
+}
