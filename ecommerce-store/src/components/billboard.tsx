c9ddec9f18cc9fe70d2cf7a266e41a3bc6794447
João Manoel <joaomanoeldev@gmail.com>
2024-02-24 16:24:42 -0300

    fix: add correct field

--- ecommerce-store/src/components/billboard.tsx
+++ ecommerce-store/src/components/billboard.tsx
@@ -11,7 +11,7 @@ export const Billboard = ({
     <div className="py-4 sm:py-6 lg:py-8 rounded-xl overflow-hidden">
       <div
         className="rounded-xl relative aspect-square md:aspect-[4.4/1] overflow-hidden bg-cover"
-        style={{ backgroundImage: `url(${data.image_url})` }}
+        style={{ backgroundImage: `url(${data.imageUrl})` }}
       >
         <div className="w-full h-full flex flex-col justify-center items-center text-center gap-y-8">
           <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
