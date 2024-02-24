9209d4c6a3086aa737cd08afebdec07e4ae78b66
João Manoel <joaomanoeldev@gmail.com>
2024-02-24 01:18:27 -0300

    feat: create NoResults component

---
+++ ecommerce-store/src/components/ui/no-results.tsx
@@ -0,0 +1,7 @@
+export const NoResults = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full text-muted-foreground">
+      Nenhum resultado encontrado.
+    </div>
+  )
+}
