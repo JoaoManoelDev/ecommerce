b9245986c3b4355ec8402cbf5c916c8469b50f30
João Manoel <joaomanoeldev@gmail.com>
2024-02-24 01:57:57 -0300

    feat: create Billboard component

---
+++ ecommerce-store/src/components/billboard.tsx
@@ -0,0 +1,24 @@
+import { Billboard as BillboardType } from "@/entities/billboard"
+
+interface BillboardProps {
+  data: BillboardType
+}
+
+export const Billboard = ({
+  data
+}: BillboardProps) => {
+  return (
+    <div className="py-4 sm:py-6 lg:py-8 rounded-xl overflow-hidden">
+      <div
+        className="rounded-xl relative aspect-square md:aspect-[4.4/1] overflow-hidden bg-cover"
+        style={{ backgroundImage: `url(${data.image_url})` }}
+      >
+        <div className="w-full h-full flex flex-col justify-center items-center text-center gap-y-8">
+          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+            <p className="text-secondary">{data.label}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
