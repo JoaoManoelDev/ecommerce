9159db78b9709b0419a4526b7656820f45b5a576
João Manoel <joaomanoeldev@gmail.com>
2024-02-24 02:05:44 -0300

    feat: add Billboard and ProductList component in HomePage

--- ecommerce-store/src/app/(root)/page.tsx
+++ ecommerce-store/src/app/(root)/page.tsx
@@ -1,11 +1,25 @@
-import { Button } from "@/components/ui/button"
+import { getBillboard } from "@/actions/billboard"
+import { getProducts } from "@/actions/products"
+import { Billboard } from "@/components/billboard"
+import { ProductList } from "@/components/products-list"
+import { Container } from "@/components/ui/container"
 
-const HomePage = () => {
+export const revalidate = 0
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true })
+  const billboard = await getBillboard("62139e51-265b-46a4-9d60-59d40d8e2914")
+
   return (
-    <>
-      <h1>Home Page</h1>
-      <Button>Click Me</Button>
-    </>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-2 lg:px-0">
+          <ProductList title="Produtos em destaque" products={products} />
+        </div>
+      </div>
+    </Container>
   )
 }
 
