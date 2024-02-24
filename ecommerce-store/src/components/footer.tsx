5c780bb76d1a9b475648925bc461b26a3e368ea9
João Manoel <joaomanoeldev@gmail.com>
2024-02-24 01:58:11 -0300

    feat: create Footer component

---
+++ ecommerce-store/src/components/footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-sm">
+          &copy; 2023 João Manoel Store, todos os direitos reservados.
+        </p>
+      </div>
+    </footer>
+  )
+}
