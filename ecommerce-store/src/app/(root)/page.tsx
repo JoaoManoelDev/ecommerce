7d7aa076457732ddcffded916f99d720a5c3db11
João Manoel <joaomanoeldev@gmail.com>
2024-02-26 13:48:59 -0300

    styles: remove px

--- ecommerce-store/src/app/(root)/page.tsx
+++ ecommerce-store/src/app/(root)/page.tsx
@@ -8,14 +8,14 @@ export const revalidate = 0
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true })
-  const billboard = await getBillboard("62139e51-265b-46a4-9d60-59d40d8e2914")
+  const billboard = await getBillboard("57cdd6fa-9d5f-41d2-900d-91f7009c4e00")
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
 
-        <div className="flex flex-col gap-y-8 px-2 lg:px-0">
+        <div className="flex flex-col gap-y-8">
           <ProductList title="Produtos em destaque" products={products} />
         </div>
       </div>
