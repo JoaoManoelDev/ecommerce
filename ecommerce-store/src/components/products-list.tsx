b0a5d02ebe8cd061619b3206d7a80c3a791cbe37
João Manoel <joaomanoeldev@gmail.com>
2024-02-26 14:01:10 -0300

    styles: add responsive carousel

--- ecommerce-store/src/components/products-list.tsx
+++ ecommerce-store/src/components/products-list.tsx
@@ -8,6 +8,7 @@ import {
   CarouselPrevious
 } from "@/components/ui/carousel"
 import { Product } from "@/entities/product"
+import { Icons } from "@/components/icons"
 
 interface ProductListProps {
   title: string
@@ -21,21 +22,25 @@ export const ProductList = ({
 
   return (
     <div className="space-y-4">
-      <h3 className="font-bold text-3xl">{title}</h3>
+      <h3 className="font-bold text-3xl flex items-center gap-x-2">
+        <span>{title}</span>
+        <Icons.arrowRight className="w-6 h-6 md:hidden" />
+      </h3>
 
       {products.length === 0 && <NoResults />}
 
-      <Carousel>
-        <CarouselContent >
+      <Carousel className="md:mx-10 py-2 h-full">
+        <CarouselContent>
           {products.map(product => (
-            <CarouselItem key={product.id} className="md:basis-1/2 lg:basis-1/5">
+            <CarouselItem key={product.id} className="basis-1/1 md:basis-1/3 lg:basis-1/5">
               <ProductCard product={product} />
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselNext />
-        <CarouselPrevious />
+        <CarouselNext variant="default" className="hidden md:flex" />
+        <CarouselPrevious variant="default" className="hidden md:flex" />
       </Carousel>
+
     </div>
   )
 }
