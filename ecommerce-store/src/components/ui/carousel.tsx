4ccd4d3a972abc043d9bb5ad243cd205d71d728f
João Manoel <joaomanoeldev@gmail.com>
2024-02-26 13:36:28 -0300

    styles: modify position and size of buttons

--- ecommerce-store/src/components/ui/carousel.tsx
+++ ecommerce-store/src/components/ui/carousel.tsx
@@ -206,9 +206,9 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-10 w-10 rounded-full",
+        "absolute h-8 w-8 rounded-full",
         orientation === "horizontal"
-          ? "-left-5 top-1/2 -translate-y-1/2"
+          ? "-left-10 top-1/2 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
         className
       )}
@@ -235,9 +235,9 @@ const CarouselNext = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-10 w-10 rounded-full",
+        "absolute h-8 w-8 rounded-full",
         orientation === "horizontal"
-          ? "-right-5 top-1/2 -translate-y-1/2"
+          ? "-right-10 top-1/2 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
         className
       )}
