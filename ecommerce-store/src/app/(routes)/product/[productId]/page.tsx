1242f32d6de7d63fd2e0dcb28c6989418d783975
João Manoel <joaomanoeldev@gmail.com>
2024-02-27 17:20:15 -0300

    feat: add info to product page

--- ecommerce-store/src/app/(routes)/product/[productId]/page.tsx
+++ ecommerce-store/src/app/(routes)/product/[productId]/page.tsx
@@ -2,6 +2,7 @@ import { Container } from "@/components/ui/container"
 import { getProduct, getProducts } from "@/actions/products"
 import { ProductList } from "@/components/products-list"
 import { Gallery } from "@/components/gallery"
+import { Info } from "@/components/info"
 
 interface ProductPageProps {
   params: {
@@ -23,7 +24,7 @@ const ProductPage = async ({
         <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
           <Gallery images={product.images} />
           <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-            Info
+            <Info product={product} />
           </div>
         </div>
 
