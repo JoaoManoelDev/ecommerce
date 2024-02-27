f13dde1af22148266b2edc9aa320cc144c7ae543
João Manoel <joaomanoeldev@gmail.com>
2024-02-26 22:34:31 -0300

    feat: create product page

---
+++ ecommerce-store/src/app/(routes)/product/[productId]/page.tsx
@@ -0,0 +1,39 @@
+import { Container } from "@/components/ui/container"
+import { getProduct, getProducts } from "@/actions/products"
+import { ProductList } from "@/components/products-list"
+import { Gallery } from "@/components/gallery"
+
+interface ProductPageProps {
+  params: {
+    productId: string
+  }
+}
+
+const ProductPage = async ({
+  params
+}: ProductPageProps) => {
+  const product = await getProduct(params.productId)
+  const suggestedProducts = await getProducts({
+    categoryId: product.category.id,
+  })
+
+  return (
+    <Container>
+      <div className="py-8 space-y-24">
+        <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+          <Gallery images={product.images} />
+          <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+            Info
+          </div>
+        </div>
+
+        <ProductList
+          title="Artigos relacionados"
+          products={suggestedProducts}
+        />
+      </div>
+    </Container>
+  )
+}
+
+export default ProductPage
