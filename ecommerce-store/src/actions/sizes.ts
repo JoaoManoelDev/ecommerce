577cdbd8e40d042f632d121e58066ab599b2a8c4
João Manoel <joaomanoeldev@gmail.com>
2024-02-29 20:55:17 -0300

    feat: create getSizes action

---
+++ ecommerce-store/src/actions/sizes.ts
@@ -0,0 +1,9 @@
+import { Size } from "@/entities/size"
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/sizes`
+
+export const getSizes = async (): Promise<Size[]> => {
+  const response = await fetch(URL)
+
+  return response.json()
+}
