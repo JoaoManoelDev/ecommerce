f80e43c27a7387579846e33affeb5a74379a3eaf
João Manoel <joaomanoeldev@gmail.com>
2024-02-29 20:55:03 -0300

    feat: create getColors action

---
+++ ecommerce-store/src/actions/colors.ts
@@ -0,0 +1,9 @@
+import { Color } from "@/entities/color"
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`
+
+export const getColors = async (): Promise<Color[]> => {
+  const response = await fetch(URL)
+
+  return response.json()
+}
