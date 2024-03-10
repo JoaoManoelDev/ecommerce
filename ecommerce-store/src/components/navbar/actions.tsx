d3fb81db86be633e3d0922ab2d45bf837ff37db5
João Manoel <joaomanoeldev@gmail.com>
2024-03-10 14:38:37 -0300

    feat: add quantity products to cart and push to cart with click

--- ecommerce-store/src/components/navbar/actions.tsx
+++ ecommerce-store/src/components/navbar/actions.tsx
@@ -1,19 +1,27 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation"
+
 import { Icons } from "@/components/icons"
+import { Button } from "@/components/ui/button"
 import { useMounted } from "@/hooks/use-mounted"
+import { useCart } from "@/hooks/use-cart"
 
 export const NavbarActions = () => {
   const { mounted } = useMounted()
+  const cart = useCart()
+  const router = useRouter()
 
   if (!mounted) return null
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full">
+      <Button
+        onClick={() => router.push("/cart")}
+        className="flex items-center rounded-full"
+      >
         <Icons.shoppingBag className="w-5 h-5" />
-        <span className="ml-2 text-sm font-medium">0</span>
+        <span className="ml-2 text-sm font-medium">{cart.products.length}</span>
       </Button>
     </div>
   )
